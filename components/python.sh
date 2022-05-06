58ce182954adf53dd496ef40e698064b189f1fc9
kharagos <kharagos@cmtelematics.com>
2022-05-06 18:05:22 +0200

    pyenv correct path

--- components/python.sh
+++ components/python.sh
@@ -3,4 +3,6 @@ eval "$(pyenv virtualenv-init -)"
 
 # eval "$(pyenv init --path)"
 
-export PATH="$HOME/.pyenv/versions/anaconda3-2021.11/bin:$PATH"
+export PATH="$HOME/.pyenv/bin:$HOME/.pyenv/shims:$PATH"
+
+#export PATH="$PATH:$HOME/.pyenv/versions/anaconda3-2021.11/bin"
